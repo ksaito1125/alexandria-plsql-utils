fac70c148c7de40924efb7739f0dc2a7141c86cf
Saito Kazuo <ksaito11@gmail.com>
2017-09-22 23:06:00 +0900

    Added create network acl script

---
+++ setup/s3_grants.sql
@@ -0,0 +1,21 @@
+grant execute on dbms_crypto to &1;
+grant execute on sys.utl_file to &1;
+
+BEGIN
+  DBMS_NETWORK_ACL_ADMIN.CREATE_ACL(acl         => 's3.xml',
+                                    description => 'S3 ACL',
+                                    principal   => '&1',
+                                    is_grant    => true,
+                                    privilege   => 'connect');
+
+  DBMS_NETWORK_ACL_ADMIN.ADD_PRIVILEGE(acl       => 's3.xml',
+                                       principal => '&1',
+                                       is_grant  => true,
+                                       privilege => 'resolve');
+
+  DBMS_NETWORK_ACL_ADMIN.ASSIGN_ACL(acl  => 's3.xml',
+                                    host => '*.s3.amazonaws.com');
+END;
+/
+COMMIT;
+
