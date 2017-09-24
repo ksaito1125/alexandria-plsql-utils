5c4cf984b9ba6c2854634400ad78b32f239d90f6
Saito Kazuo <ksaito11@gmail.com>
2017-09-24 21:39:10 +0900

    Added sample

---
+++ demos/s3_sample.sql
@@ -0,0 +1,10 @@
+begin
+  amazon_aws_auth_pkg.init ('&1', '&2', p_gmt_offset => -1);
+end;
+/
+
+begin
+  debug_pkg.debug_on;
+  amazon_aws_s3_pkg.new_bucket('xxxxxmy-bucket-name', amazon_aws_s3_pkg.g_region_asia_pacific_tokyo);
+end;
+/
